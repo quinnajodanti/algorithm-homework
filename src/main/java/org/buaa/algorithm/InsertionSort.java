8b5eca2809a42b3afaa5057413cc040b4dfae4cb
zhangsw <zhangsw1412.cpp@gmail.com>
2018-05-04 15:17:52 +0800

    插入排序

---
+++ src/main/java/org/buaa/algorithm/InsertionSort.java
@@ -0,0 +1,35 @@
+package org.buaa.algorithm;
+
+import java.util.Arrays;
+
+public class InsertionSort
+{
+	public static void InsertSort(int[] arr)
+	{
+		int i, j;
+		int n = arr.length;
+		int target;
+
+		for (i = 1; i < n; i++)
+		{
+			j = i;
+			target = arr[i];
+
+			while (j > 0 && target < arr[j - 1])
+			{
+				arr[j] = arr[j - 1];
+				j--;
+			}
+
+			arr[j] = target;
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		int[] array = new int[]{6, 2, 7, 3, 4, 1, 9, 0, 5, 8};
+		System.out.println("Before sort:" + Arrays.toString(array));
+		InsertionSort.InsertSort(array);
+		System.out.println("After sort:" + Arrays.toString(array));
+	}
+}
