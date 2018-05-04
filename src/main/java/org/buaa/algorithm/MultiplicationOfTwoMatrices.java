38bb0ef56e04503db14e6966d07f1431a453a502
zhangsw <zhangsw1412.cpp@gmail.com>
2018-05-04 16:07:21 +0800

    去年注释

--- src/main/java/org/buaa/algorithm/MultiplicationOfTwoMatrices.java
+++ src/main/java/org/buaa/algorithm/MultiplicationOfTwoMatrices.java
@@ -9,19 +9,16 @@ public class MultiplicationOfTwoMatrices
 {
 	public static int[][] matrix(int a[][], int b[][])
 	{
-		//当a的列数与矩阵b的行数不相等时，不能进行点乘，返回null
 		if (a[0].length != b.length)
 		{
 			return null;
 		}
-		//c矩阵的行数y，与列数x
 		int y = a.length;
 		int x = b[0].length;
 		int c[][] = new int[y][x];
 		for (int i = 0; i < y; i++)
 		{
 			for (int j = 0; j < x; j++)
-			//c矩阵的第i行第j列所对应的数值，等于a矩阵的第i行分别乘以b矩阵的第j列之和
 			{
 				for (int k = 0; k < b.length; k++)
 				{
